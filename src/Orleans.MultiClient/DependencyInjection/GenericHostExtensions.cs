5a37f4f25f6aeedbf40e11ae811919f101d5e255
lfzm <510415008@qq.com>
2019-06-13 09:43:13 +0800

    add generic host extensions

---
+++ src/Orleans.MultiClient/DependencyInjection/GenericHostExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Orleans.MultiClient.DependencyInjection;
+using System;
+
+namespace Microsoft.Extensions.Hosting
+{
+    public static class GenericHostExtensions
+    {
+        public static IHostBuilder UseOrleansMultiClient(this IHostBuilder hostBuilder, Action<IMultiClientBuilder> startup)
+        {
+            hostBuilder.ConfigureServices((context, service) => service.AddOrleansMultiClient(startup));
+            return hostBuilder;
+        }
+    }
+}
